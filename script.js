09ac17ea8d83a3120c5c8d31ecbf6f9db0c43f35
George Yu <llorch@Georges-MacBook-Pro.local>
2018-02-22 23:25:31 -0800

    Add animation and documentation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// DOM element selection
 let mainSquare = document.getElementById("main-square");
 let square = document.getElementsByClassName("square");
 
+// Small colored square click event
 for(let i=0; i<square.length; i++){
 	square[i].addEventListener("click", function(){
+		// Taking CSS property from small colored square to add it to big quare, window.getComputedStyle(this) is needed because of external css file.
 		mainSquare.style.backgroundColor = window.getComputedStyle(this).getPropertyValue("background-color");
 	});
 }
